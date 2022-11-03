921659c7fe75b887365a1cc69f5ac9627c60ea21
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-11-03 21:25:30 +0100

    Add more tests for organizer and main
    Also switched to pathlib

--- tests/test_organizer.py
+++ tests/test_organizer.py
@@ -1,5 +1,7 @@
 from logging import exception
+import pathlib
 import shutil
+import time
 import pytest
 import io
 import marshal
@@ -7,11 +9,11 @@ import os
 from fotool import organizer
 
 sep = os.path.sep
-samples_base = f"tests{sep}samples"
+samples_base = pathlib.Path(f"tests{sep}samples")
 samples_files = [
-    os.path.join(samples_base, "vid.mp4"),
-    os.path.join(samples_base, "img.png"),
-    os.path.join(samples_base, "1.txt"),
+    samples_base/"vid.mp4",
+    samples_base/"img.png",
+    samples_base/"1.txt",
 ]
 samples_types = [("mp4", "MP4 Files"), ("png", "PNG Files"), ("txt", "Text File")]
 samples_files_type = dict(zip(samples_files, samples_types))
@@ -66,6 +68,10 @@ def test_default_write_action_log():
 
 
 def test_default_add_to_log():
+    try:
+        os.remove("action_log")
+    except Exception:
+        pass
     org = organizer.DefaultOrganizer(reversible=True)
     data = ["write", "old", "new"]
     org.action_logs = {}
@@ -74,10 +80,10 @@ def test_default_add_to_log():
     assert org.action_logs[key] == [data[0], os.path.abspath(data[1]), data[2]]
 
     org = organizer.DefaultOrganizer(reversible=True)
-    from_ = os.path.abspath(os.path.join(samples_base, "img.png"))
+    from_ = (samples_base/"img.png").absolute()
     org.default_add_to_log("print", from_, samples_base)
     key = list(org.action_logs.keys())[0]
-    assert org.action_logs[key] == ["print", from_, from_]
+    assert org.action_logs[key] == ["print", str(from_), str(from_)]
 
 
 @pytest.mark.parametrize("file", samples_files)
@@ -143,7 +149,7 @@ def test_default_generate_destination_group(file):
 
 
 @pytest.mark.parametrize(
-    "file", samples_files + [os.path.join(samples_base, "action_log")]
+    "file", samples_files + [samples_base/"action_log"]
 )
 def test_default_generate_destination_group_nomatch(file):
     org = organizer.DefaultOrganizer(reversible=True)
@@ -154,30 +160,30 @@ def test_default_generate_destination_group_nomatch(file):
 
 
 @pytest.mark.parametrize(
-    "file", samples_files + [os.path.join(samples_base, "action_log")]
+    "file", samples_files + [samples_base/"action_log"]
 )
 def test_default_generate_destination_group_nomatch_dd(file):
     org = organizer.DefaultOrganizer(reversible=True)
-    nomatchdir = os.path.join("[:dd:]", "nomatch")
+    nomatchdir = pathlib.Path("[:dd:]")/"nomatch"
     assert f"{sep}home" + samples_files_groups.get(file, f"{sep}nomatch").replace(
         ".", ""
     ) == org.default_generate_destination_group(
-        file, f"{sep}home", nomatchdir=nomatchdir
+        file, f"{sep}home", nomatchdir=str(nomatchdir)
     )
 
 
 def test_default_copy():
     org = organizer.DefaultOrganizer(reversible=True)
-    to = os.path.join(samples_base, "4.txt")
-    from_ = os.path.join(samples_base, "1.txt")
+    to = samples_base/"4.txt"
+    from_ = samples_base/"1.txt"
     assert org.default_copy(from_, to) == to
-    assert os.path.exists(to) and os.path.exists(from_)
+    assert to.exists() and from_.exists()
 
     assert org.default_copy(to, from_, move=True) == -1
     assert org.default_copy(to, from_, move=True, overwrite=True) == from_
-    assert os.path.exists(to) is False
+    assert to.exists() is False
 
-    assert org.default_copy(from_, samples_base, move=True, overwrite=True) == from_
+    assert org.default_copy(from_, samples_base, move=True, overwrite=True) == str(from_)
     try:
         assert org.default_copy("~/does-not-exist", ".", overwrite=True) == -1
         assert (
@@ -189,11 +195,11 @@ def test_default_copy():
 
 def test_default_rename():
     org = organizer.DefaultOrganizer(reversible=True)
-    to = os.path.join(samples_base, "rename2.txt")
-    from_ = os.path.join(samples_base, "rename.txt")
+    to = samples_base/"rename2.txt"
+    from_ = samples_base/"rename.txt"
     assert org.default_rename(from_, to) == to
     assert org.default_rename(to, from_, copy=True) == from_
-    assert os.path.exists(to) and os.path.exists(from_)
+    assert to.exists() and from_.exists()
 
     assert org.default_rename(to, from_) == -1
     assert org.default_rename(to, from_, overwrite=True) == from_
@@ -245,7 +251,7 @@ def test_default_simplesearch_case(search, string):
 
 def test_default_walk_dir():
     org = organizer.DefaultOrganizer(reversible=True)
-    path = os.path.join(samples_base, "walk")
+    path = samples_base/"walk"
     assert set(org.default_walk_dir(path)) == {
         f"tests{sep}samples{sep}walk{sep}1.txt",
         f"tests{sep}samples{sep}walk{sep}vid.mp4",
@@ -255,7 +261,6 @@ def test_default_walk_dir():
         f"tests{sep}samples{sep}walk{sep}2.txt",
         f"tests{sep}samples{sep}walk{sep}img.png",
     }
-
     assert set(org.default_walk_dir(path, extensions=["png"])) == {
         f"tests{sep}samples{sep}walk{sep}img.png",
     }
@@ -263,7 +268,7 @@ def test_default_walk_dir():
 
 def test_default_walk_dir_recursive():
     org = organizer.DefaultOrganizer(reversible=True)
-    path = os.path.join(samples_base, "inner")
+    path = samples_base/"inner"
     assert (
         len(
             set((org.default_walk_dir_recursive(path))).difference(
@@ -323,61 +328,61 @@ def test_default_generate_groups():
     assert org.groups == ["5"]
 
 def create_action_files(count):
-    base = os.path.join(samples_base, "actions")
+    base = samples_base/"actions"
     shutil.rmtree(base)
     os.mkdir(base)
     paths = []
     for i in range(count):
-        path = os.path.join(base, f"{i}.txt")
+        path = base/f"{i}.txt"
         with open(path, "w") as f:
             f.write("Whatever")
         paths.append(path)
     return paths
 
 def test_default_action_copy():
-    base = os.path.join(samples_base, "actions")
+    base = samples_base/"actions"
     org = organizer.DefaultOrganizer(reversible=True)
     paths = create_action_files(1)
-    target = os.path.join(base, "hmm.txt")
+    target = base/"hmm.txt"
     res = org.default_action(paths[0], target, "copy")
     assert res == target
     assert os.path.isfile(res) and os.path.isfile(paths[0])
 
 def test_default_action_rename():
-    base = os.path.join(samples_base, "actions")
+    base = samples_base/"actions"
     org = organizer.DefaultOrganizer(reversible=True)
     paths = create_action_files(1)
-    target = os.path.join(base, "hmm.txt")
+    target = base/"hmm.txt"
     res = org.default_action(paths[0], target, "rename")
     assert res == target
     assert os.path.isfile(res) and not os.path.isfile(paths[0])
 
 def test_default_action_copy_rename():
-    base = os.path.join(samples_base, "actions")
+    base = samples_base/"actions"
     org = organizer.DefaultOrganizer(reversible=True)
     paths = create_action_files(1)
-    target = os.path.join(base, "hmm.txt")
+    target = base/"hmm.txt"
     res = org.default_action(paths[0], target, "copy_rename")
     assert res == target
     assert os.path.isfile(res) and os.path.isfile(paths[0])
 
 def test_default_action_move():
-    base = os.path.join(samples_base, "actions")
+    base = samples_base/"actions"
     org = organizer.DefaultOrganizer(reversible=True)
     paths = create_action_files(1)
-    target = os.path.join(base, "hmm.txt")
+    target = base/"hmm.txt"
     res = org.default_action(paths[0], target, "move")
     assert res == target
     assert os.path.isfile(res) and not os.path.isfile(paths[0])
 
 def test_default_action_print():
-    base = os.path.join(samples_base, "actions")
+    base = samples_base/"actions"
     org = organizer.DefaultOrganizer(reversible=True)
     paths = create_action_files(1)
     _ = org.default_action(paths[0], "", "print")
     assert os.path.isfile(paths[0])
 
-    target = os.path.join(base, "hmm.txt")
+    target = base/"hmm.txt"
     _ = org.default_action(paths[0], target, "print")
     assert os.path.isfile(paths[0])
 
@@ -402,6 +407,130 @@ def test_default_reverse_no_actionfile():
     except FileNotFoundError:
         pass
 
-# def test_default_reverse():
-#     org = organizer.DefaultOrganizer(reversible=True)
-#     org.default_copy()
+def test_default_reverse_move():
+    os.remove("action_log")
+    org = organizer.DefaultOrganizer(reversible=True)
+    to = samples_base/"reverse"/"to"
+    fname = "21.txt"
+    try:
+        (to/fname).unlink()
+    except:
+        pass
+    org.default_action(samples_base/"reverse"/fname, to, "move")
+    org.default_write_action_log()
+    assert (to/fname).exists()
+
+    org = organizer.DefaultOrganizer(reverse=True)
+    org.default_reverse()
+    assert (to/fname).exists() is False
+
+def test_default_reverse():
+    os.remove("action_log")
+    org = organizer.DefaultOrganizer(reversible=True)
+    to = samples_base/"reverse"/"to"
+    fname = "21.txt"
+    from_ = samples_base/"reverse"/fname
+    org.default_action(from_, to, "copy")
+    org.default_write_action_log()
+    assert (to/fname).exists()
+    from_.unlink()
+
+    org = organizer.DefaultOrganizer(reverse=True)
+    org.default_reverse()
+    assert from_.exists() is True
+
+def test_default_reverse_move_count():
+    os.remove("action_log")
+    org = organizer.DefaultOrganizer(reversible=True)
+    to = samples_base/"reverse"/"to"
+    fname = "21.txt"
+    fname2 = "33.txt"
+    # (to/fname).unlink()
+    from1 = samples_base/"reverse"/fname
+    from2 = samples_base/"reverse"/fname2
+    org.default_action(from1, to, "move")
+    org.default_action(from2, to, "move")
+    org.default_write_action_log()
+    assert (to/fname).exists()
+    assert (to/fname2).exists()
+
+    org = organizer.DefaultOrganizer(reverse=True, reverse_count=1)
+    org.default_reverse()
+    assert from1.exists() is False
+    assert from2.exists()
+    org.default_action(to/fname, samples_base/"reverse", "move")
+
+def test_default_reverse_timestampstart():
+    os.remove("action_log")
+    org = organizer.DefaultOrganizer(reversible=True)
+    to = samples_base/"reverse"/"to"
+    fname = "21.txt"
+    fname2 = "33.txt"
+    # (to/fname).unlink()
+    from1 = samples_base/"reverse"/fname
+    from2 = samples_base/"reverse"/fname2
+    org.default_action(from1, to, "move")
+    time.sleep(0.5)
+    t2 = time.time()
+    org.default_action(from2, to, "move")
+    org.default_write_action_log()
+    assert (to/fname).exists()
+    assert (to/fname2).exists()
+
+    org = organizer.DefaultOrganizer(reverse=True, reversetimerangestart=t2)
+    org.default_reverse()
+    assert from1.exists() is False
+    assert from2.exists()
+    org.default_action(to/fname, samples_base/"reverse", "move")
+
+def test_default_reverse_timestampend():
+    os.remove("action_log")
+    org = organizer.DefaultOrganizer(reversible=True)
+    to = samples_base/"reverse"/"to"
+    fname = "21.txt"
+    fname2 = "33.txt"
+    # (to/fname).unlink()
+    from1 = samples_base/"reverse"/fname
+    from2 = samples_base/"reverse"/fname2
+    org.default_action(from1, to, "move")
+    t2 = time.time()
+    time.sleep(0.5)
+    org.default_action(from2, to, "move")
+    org.default_write_action_log()
+    assert (to/fname).exists()
+    assert (to/fname2).exists()
+
+    org = organizer.DefaultOrganizer(reverse=True, reversetimerangestop=t2)
+    org.default_reverse()
+    assert from2.exists() is False
+    assert from1.exists()
+    org.default_action(to/fname2, samples_base/"reverse", "move")
+
+def test_default_reverse_timestamprange():
+    os.remove("action_log")
+    org = organizer.DefaultOrganizer(reversible=True)
+    to = samples_base/"reverse"/"to"
+    fname = "21.txt"
+    fname2 = "33.txt"
+    fname3 = "52.txt"
+    from1 = samples_base/"reverse"/fname
+    from2 = samples_base/"reverse"/fname2
+    from3 = samples_base/"reverse"/fname3
+    org.default_action(from1, to, "move")
+    t2 = time.time()
+    time.sleep(0.5)
+    org.default_action(from2, to, "move")
+    t3 = time.time()
+    time.sleep(0.5)
+    org.default_action(from3, to, "move")
+    org.default_write_action_log()
+    assert (to/fname).exists()
+    assert (to/fname2).exists()
+
+    org = organizer.DefaultOrganizer(reverse=True, reversetimerangestop=t3, reversetimerangestart=t2)
+    org.default_reverse()
+    assert from3.exists() is False
+    assert from2.exists()
+    assert from1.exists() is False
+    org.default_action(to/fname3, samples_base/"reverse", "move")
+    org.default_action(to/fname, samples_base/"reverse", "move")
