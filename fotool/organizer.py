ba45aa3a7fbb88c8c7379c8b861b90a201227e99
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-11-03 21:29:01 +0100

    Update organizer.py

--- fotool/organizer.py
+++ fotool/organizer.py
@@ -1,37 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-# Copyright 2021 Matthew Wisdom
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from this
-# software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 import os
+import pathlib
 import re
 import os.path as pth
 import sys
@@ -180,7 +148,7 @@ class DefaultOrganizer:
             self.orderedkeys = keysaslist
             reverse_method = getattr(self, "reverse", self.default_reverse)
             reverse_method()
-            sys.exit()
+            return
 
     def default_write_action_log(self):
         if not self.reversible:
@@ -201,7 +169,7 @@ class DefaultOrganizer:
         self.action_logs[name] = [action, pth.abspath(old), new]
 
     def default_get_file_type(self, file):
-        if isinstance(file, str):
+        if isinstance(file, (pathlib.Path, str)):
             ft = filetype.guess(file)
             if not ft:
                 return self.default_get_more_types(file)
@@ -332,7 +300,7 @@ class DefaultOrganizer:
     def default_reverse(self):
         if not self.orderedkeys and self.action_logs:
             logger.error("Could not get object for reverse operations.")
-            sys.exit()
+            raise ValueError("Could not get object for reverse operations.")
         count = 0
         tookaction = False
         for i in self.orderedkeys:
@@ -421,7 +389,7 @@ class DefaultOrganizer:
         Set move to True to move instead of copy
         and set overwrite flag to overwrite existing files.
         """
-        if pth.exists(to) and not overwrite:
+        if pth.exists(to) and not overwrite and pth.isfile(to):
             return -1
 
         path, fname = pth.split(from_)
@@ -474,7 +442,7 @@ class DefaultOrganizer:
             if extensions:
                 try:
                     ext = self.default_get_file_type(fname)[0]
-                except:
+                except Exception as e:
                     continue
                 if ext in extensions:
                     yield fname
