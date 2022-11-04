2a3e4ade88be0ca4b172a38b1705e4a33cbd08d0
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-11-04 19:58:26 +0100

    update pcloud extension

--- extensions/pcloud_upload.py
+++ extensions/pcloud_upload.py
@@ -9,10 +9,10 @@ import os
 
 from pcloud import PyCloud
 
-pc = PyCloud(os.getenv("PCLOUD_USER"), os.getenv("PCLOUD_PASS"))
 
 class PcloudUploadExtension:
     def action(self, from_, to="", action="print"):
+        pc = PyCloud(os.getenv("PCLOUD_USER"), os.getenv("PCLOUD_PASS"))
         if action == "upload":
             pc.uploadfile(files=[from_])
         self.default_action(from_, to, action)
