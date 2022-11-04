4b4adbe8d0c9c9808df1f7e70f80fd48902a57a7
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-11-04 19:38:36 +0100

    Add pcloud_upload extension

---
+++ extensions/pcloud_upload.py
@@ -0,0 +1,17 @@
+"""
+    Upload matching files to pcloud.
+
+    Requires pycloud to be installed.
+
+    `pip install pcloud`
+"""
+import os
+from pcloud import PyCloud
+
+pc = PyCloud(os.getenv("PCLOUD_USER"), os.getenv("PCLOUD_PASS"))
+
+class PcloudUploadExtension:
+    def action(self, from_, to="", action="print"):
+        if action == "upload":
+            pc.uploadfile(files=[from_])
+        self.default_action(from_, to, action)
